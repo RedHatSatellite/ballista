cc89093348449dbd2d17fc3d722f70b4b541bba2
Yoram Hekma <hekma.yoram@gmail.com>
2016-06-05 16:01:59 +0200

    add docstrings to katello_helpers

--- katlibs/main/katello_helpers.py
+++ katlibs/main/katello_helpers.py
@@ -20,6 +20,20 @@ import json
 
 class KatelloConnection(object):
     def __init__(self, base_url, username, password, verify, organization):
+        """
+        :param base_url: Url to connect to
+        :type base_url: str
+        :param username: Username for querying
+        :type username: str
+        :param password: Password for querying
+        :type password: str
+        :param verify: Whether to accept self-signed ssl
+        :type verify: bool
+        :param organization: Organization to use
+        :type organization: str
+        :returns: KatelloConnection object
+        :rtype: KatelloConnection
+        """
         self.organization = organization
         self.base_url = base_url
         self.verify = verify
@@ -31,6 +45,15 @@ class KatelloConnection(object):
             requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
     def _query_api(self, url):
+        """
+        Succesively query the API, appending "?page=<int>" to the url, each time incrementing the int by one.
+        When we get no results, return the collected data.
+
+        :param url: Url that needs to be concatenated to the base_url
+        :type url: str
+        :return: List of dictionaries as returned from the api ['results']
+        :rtype: list
+        """
         if url.endswith('/'):
             url = url[:-1]
 
@@ -47,6 +70,14 @@ class KatelloConnection(object):
         return results
 
     def __getattr__(self, item):
+        """
+        Dynamically defer the requested attribute to the API
+
+        :param item: The attribute we want (content_views, organizations, etc)
+        :type item: str
+        :return: List of dictionaries as returned from the api call ['results']
+        :rtype: list
+        """
         if item == 'orgid':
             if 'orgid' not in self.__dict__:
                 self.orgid = self._get_orgid()
@@ -61,6 +92,16 @@ class KatelloConnection(object):
             return self._get_foreman_dict('%s/' % item)
 
     def _get_katello_dict(self, uri, clean=False):
+        """
+        Call the katello api (https://url/katello/api/v2)
+
+        :param uri: Url to query
+        :type uri: str
+        :param clean: When true, we do not want to use the organization part in the url
+        :type clean: bool
+        :return: List of dictionaries as returned from the api call ['results']
+        :rtype: list
+        """
         if not uri == 'organizations/' and not clean:
             try:
                 return self._query_api('%s/katello/api/v2/organizations/%s/%s' % (self.base_url, self.orgid, uri))
@@ -70,14 +111,34 @@ class KatelloConnection(object):
         return self._query_api('%s/katello/api/v2/%s' % (self.base_url, uri))
 
     def _get_foreman_dict(self, uri):
+        """
+        Call the foreman api (https://url/api/v2)
+
+        :param uri: Url to query
+        :return: List of dictionaries as returned from the api call ['results']
+        :rtype: list
+        """
         return self._query_api('%s/api/v2/%s' % (self.base_url, uri))
 
     def _get_orgid(self):
+        """
+        Get the organizational id of our organization
+
+        :rtype: int
+        """
         for org in self.organizations:
             if org['name'] == self.organization:
                 return org['id']
 
     def get_collection_contents(self, collection_name):
+        """
+        Get the hosts that belong to a host_collection
+
+        :param collection_name: Name of the host_collection to query
+        :type collection_name: str
+        :return: List of host dicts
+        :rtype: list
+        """
         col_id = [h['id'] for h in self.host_collections if h['name'] == collection_name][0]
         return self._get_katello_dict('host_collections/%s/systems' % col_id, clean=True)
 
@@ -103,11 +164,20 @@ class KatelloConnection(object):
 
 
 def get_components(datalist, index):
-    # Given a list of dictionaries, return the first key encountered in the first dict
-    # so given datalist =
-    # [{'name': 'name1', 'val1': 'val1'}, {'name': 'name2', 'val': 'val2'}]
-    # we can get only the second item:
-    # get_components(datalist, ('name', 'name2'))
+    """
+    Given a list of dictionaries, return the first key encountered in the first dict
+    so given datalist =
+    [{'name': 'name1', 'val1': 'val1'}, {'name': 'name2', 'val': 'val2'}]
+    we can get only the second item:
+    get_components(datalist, ('name', 'name2'))
+
+    :param datalist: List of dictionaries to search in
+    :type datalist: list
+    :param index: Tuple to search for. Index0 is the key, Index1 the value
+    :type index: tuple
+    :return: The dictionary that matches when found, else None
+    :rtype: None or dict
+    """
 
     search_key = index[0]
     search_val = index[1]
