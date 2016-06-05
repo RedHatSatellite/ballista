a88c8b3aa9124719a02dd7d0bde4e017d6e6357a
Yoram Hekma <hekma.yoram@gmail.com>
2016-06-05 14:43:32 +0200

    first attempt at repl

---
+++ katlibs/modules/repl.py
@@ -0,0 +1,22 @@
+import cmd
+from pprint import pprint
+
+
+class Katloop(cmd.Cmd):
+    def __init__(self, connection):
+        cmd.Cmd.__init__(self)
+        self.connection = connection
+
+    def do_list_cviews(self, line):
+        pprint(self.connection.content_views)
+
+    def do_list_versions(self, version_name):
+        pass
+
+    def do_EOF(self, line):
+        return True
+
+
+def main(**kwargs):
+    loop = Katloop(kwargs['connection'])
+    loop.cmdloop()
